b3046b44b1cbeb10c7bdfc52fdbdf686d18f29a6
lepoissondargile <lepoissondargile@gmail.com>
2015-05-16 15:55:26 +0200

    ajout attribut async sur scripts non prioritaires
    conseils GTmetrix

--- View/php/footer.php
+++ View/php/footer.php
@@ -2,14 +2,14 @@
 </body>
  <foot>
     <script type="text/javascript" src="View/js/jquery.min.js"></script>
-    <script type="text/javascript" src="View/js/jquery.qrcode.min.js"></script>
+    <script type="text/javascript" src="View/js/jquery.qrcode.min.js" async></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script type="text/javascript" src="View/js/bootstrap.min.js"></script>
     <!--<script type='text/javascript' src='../js/pageNotConnected.js'></script> CKE seulement pour pageNotConnected-->
     <script type='text/javascript' src='View/js/recherche.js'></script> 
    	<script type="text/javascript" src="View/js/pdfMaker.js"></script>
    	<!--script type="text/javascript" src="View/js/jsPDF/jspdf.js"></script-->
-	<script type="text/javascript" src="View/js/jsPDF/dist/jspdf.debug.js"></script>
+	<script type="text/javascript" src="View/js/jsPDF/dist/jspdf.debug.js" async></script>
 	<!--script type="text/javascript" src="View/js/jsPDF/jspdf.plugin.standard_fonts_metrics.js"></script>
 	<script type="text/javascript" src="View/js/jsPDF/jspdf.plugin.from_html.js"></script>
 	<script type="text/javascript" src="View/js/jsPDF/jspdf.plugin.addimage.js"></script>
