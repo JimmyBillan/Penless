1e42fe43fa6316682ef8887d8181a8c4814c634f
jimmyBillan <billan.jimmy@gmail.com>
2015-05-16 23:00:40 +0200

    Import js fixed

--- View/php/pageConnected.php
+++ View/php/pageConnected.php
@@ -18,17 +18,15 @@ include('sidebarConnected.php');
 
 echo"</div>";  
 
-include('footer.php'); /* scripts communs Connected/NotConnected */
+/* Footer */
 
-echo' <script type="text/javascript" src="View/js/editor2/lodash.js"></script>
- <script type="text/javascript" src="View/js/editor2/quill.js"></script>
- <script type="text/javascript" src="View/js/editor2/advanced.js"></script>';*/
+include('footer.php');
+echo  "<script type='text/javascript' src='View/js/user.js'></script>";
+echo  "<script type='text/javascript' src='View/js/partage.js'></script>"; 
 
 echo  '<script type="text/javascript" src="View/js/wysihtml/dist/wysihtml5-0.3.0.min.js"></script>
  <script type="text/javascript" src="View/js/wysihtml/parser_rules/advanced.js"></script>';
 
-echo  "<script type='text/javascript' src='View/js/user.js'></script>";
-echo  "<script type='text/javascript' src='View/js/partage.js'></script>"; 
-
+echo "</body>";
 echo "</html>";
 
